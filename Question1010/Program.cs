e6fb9199f8402b7b87ad60908e67739d113bf015
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-04-21 00:11:06 +0300

    Final#1Q

---
+++ Question1010/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+ 
+ void MaxCount(){
+
+    int rand_count = new Random().Next(10,100);
+    int dec = rand_count/10;
+    int count = rand_count%10;
+    Console.WriteLine("Random num = "+ rand_count);
+    if (dec>count){
+        Console.WriteLine("First num bigger " + dec);
+        //return dec;
+        }
+    else {
+       Console.WriteLine("Second num bigger " + count);
+        //return count;
+        }
+    }
+        
+// 1. Напишите метод, который выводит 
+// случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8 
+          
+
+// int rand_count = new Random().Next(10,100);
+MaxCount();
+    
