ef4ad745b99a9905109df8091d118fea8374cd41
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-04-24 22:46:29 +0300

    Create107

--- Question107/Program.cs
+++ Question107/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 /*
 7. Напишите программу, которая выводит 
 третью цифру заданного числа 
@@ -10,5 +9,29 @@
 */
 
 
+int ThirdCount(int numeric){
+    int Countnumber = 0; // колличество цифр в числе
+    int x = numeric; // копия числа
+    while (x !=0 ){
+        x /=10;        // отсеивание последней цифры 
+        Countnumber++; // подсчет 
+    }
+    int[] NumericCount = new int [Countnumber]; // массив чисел
+    int i = (NumericCount.Length -1); // корректировка индекса
+    while (i>0){
+        NumericCount[i] = (numeric%10);  // присваиваине в массив последней цифры числа
+        numeric /= 10;                   // срез последней цифры
+        i--;                   
+    } 
+    return NumericCount[2];            // возвращение 3й цифры
+}
 
+
+int num = 1859476;
+if (num>=100){
+    Console.WriteLine("Third number is {0}",ThirdCount(num));
+}
+else{
+    Console.WriteLine("3й цифры нет");
+}
 
